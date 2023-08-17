d818b38b6ed02fe8d5f25461c105750add0ad34b
JamiltonQuintero <transportador1118@gmail.com>
2023-08-17 09:29:12 -0500

    Se agrega codigo base

---
+++ StrategyExample/src/main/java/com/jamiltonquintero/strategyexample/StrategyExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jamiltonquintero.strategyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategyExampleApplication.class, args);
+	}
+
+}
---
+++ StrategyExample/src/test/java/com/jamiltonquintero/strategyexample/StrategyExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamiltonquintero.strategyexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
